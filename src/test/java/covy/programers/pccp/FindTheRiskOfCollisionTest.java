83761b2ed6465017d3808bc4a684d62525e917c8
covy <junni802@naver.com>
2024-11-17 22:56:41 +0900

    feat fix : 테스트코드 변경

--- src/test/java/covy/programers/pccp/FindTheRiskOfCollisionTest.java
+++ src/test/java/covy/programers/pccp/FindTheRiskOfCollisionTest.java
@@ -1,38 +1,89 @@
 package covy.programers.pccp;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
 
 class FindTheRiskOfCollisionTest {
 
     public int solution(int[][] points, int[][] routes) {
         int answer = 0;
-        Set<String> set = new HashSet<>();
-        for (int i=0; i<routes.length; i++) {
-            for (int j=0; j<routes[i].length; j++) {
-                System.out.print("값들을 확인해 보자 -> " + routes[i][j]);
+        List<String> list = new ArrayList<>();
+            for (int i = 0; i < routes.length; i++) {
+                int num = 1;
+                for (int j = 0; j < routes[i].length; j++) {
+                    boolean result = true;
+                    if (j != routes[i].length - 1) {
+                        int nextX = points[routes[i][j + 1] - 1][0];
+                        int nextY = points[routes[i][j + 1] - 1][1];
+                        int currentX = points[routes[i][j] - 1][0];
+                        int currentY = points[routes[i][j] - 1][1];
+                        while (result) {
+                            if (currentX < nextX){
+                                currentX++;
+                                String text = "[" + num +"]" + currentX + ", " + currentY;
+                                if (list.contains(text)) {
+                                    answer++;
+                                    list.set(list.indexOf(text), "X");
+                                }else {
+                                    list.add(text);
+                                }
+                                num++;
+                            }else if (currentX > nextX) {
+                                currentX--;
+                                String text = "[" + num +"]" + currentX + ", " + currentY;
+                                if (list.contains(text)) {
+                                    answer++;
+                                    list.set(list.indexOf(text), "X");
+                                }else {
+                                    list.add(text);
+                                }
+                                num++;
+                            } else {
+                                if (currentY < nextY) {
+                                    currentY++;
+                                    String text = "[" + num +"]" + currentX + ", " + currentY;
+                                    if (list.contains(text)) {
+                                        answer++;
+                                        list.set(list.indexOf(text), "X");
+                                    }else {
+                                        list.add(text);
+                                    }
+                                    num++;
+                                }else if (currentY > nextY) {
+                                    currentY--;
+                                    String text = "[" + num +"]" + currentX + ", " + currentY;
+                                    if (list.contains(text)) {
+                                        answer++;
+                                        list.set(list.indexOf(text), "X");
+                                    }else {
+                                        list.add(text);
+                                    }
+                                }else {
+                                    result = false;
+                                }
+                            }
+                        }
+
+                    }
+                }
             }
-            System.out.println();
-        }
+
         return answer;
     }
 
     @Test
     void solutionTest() {
         int[][] points = {
-                {4, 2},
-                {1, 3},
-                {2, 4}
+                {1, 1},  // Point 1
+                {1, 2},  // Point 2
+                {1, 3},  // Point 3
+                {2, 1},  // Point 4
+                {2, 2}   // Point 5
         };
 
         int[][] routes = {
-                {4, 2},
-                {1, 3},
-                {2, 4}
+                {1, 2, 3}, // 경로 1: Point 1 -> Point 2 -> Point 3
+                {4, 5, 3}  // 경로 2: Point 4 -> Point 5 -> Point 3 (충돌 발생)
         };
 
         // 매개변수로 전달
