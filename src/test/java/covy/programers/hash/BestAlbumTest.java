d5129c1f20769b488e59cf9802c13d4bc079b528
covy <junni802@naver.com>
2025-03-24 01:02:01 +0900

    feat: add BestAlbum Quest

---
+++ src/test/java/covy/programers/hash/BestAlbumTest.java
@@ -0,0 +1,68 @@
+package covy.programers.hash;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.junit.jupiter.api.Test;
+
+class BestAlbumTest {
+
+  @Test
+  void solution() {
+    String[] arr1 = {"classic", "pop", "classic", "classic", "pop"};
+    int[] arr2 = {100, 600, 150, 150, 2500};
+    solution1(arr1, arr2);
+  }
+
+  public int[] solution1(String[] genres, int[] plays) {
+
+    Map<String, Integer> popularGeners = new HashMap<>();
+
+    for(int i=0; i<genres.length; i++) {
+      popularGeners.put(genres[i], popularGeners.getOrDefault(genres[i], 0) + plays[i]);
+    }
+
+    List<Entry<String, Integer>> entryList = new ArrayList<>(popularGeners.entrySet());
+    entryList.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+    List<Integer> arr = new ArrayList<>();
+
+    for (int i=0; i<entryList.size(); i++) {
+      Map<Integer, Integer> num = new HashMap<>();
+      for (int j=0; j<plays.length; j++) {
+
+        if (genres[j].equals(entryList.get(i).getKey())) {
+          num.put(j, plays[j]);
+        }
+      }
+      if (num.size() == 1) {
+        for (Map.Entry<Integer, Integer> entry : num.entrySet()) {
+          arr.add(entry.getKey());
+          break;  // 첫 번째 항목만 가져오고 루프 종료
+        }
+      } else {
+        List<Map.Entry<Integer, Integer>> entryList1 = new ArrayList<>(num.entrySet());
+        entryList1.sort((a, b) -> {
+          int keyComparison = b.getValue().compareTo(a.getValue());
+          if (keyComparison == 0) {
+            return a.getKey().compareTo(b.getKey());
+          }
+          return keyComparison;
+        });
+        arr.add(entryList1.get(0).getKey());
+        arr.add(entryList1.get(1).getKey());
+      }
+    }
+
+    int[] answer = new int[arr.size()];
+    for (int i=0; i<arr.size(); i++) {
+      answer[i] = arr.get(i);
+      System.out.println(arr.get(i));
+    }
+
+    return answer;
+  }
+}
