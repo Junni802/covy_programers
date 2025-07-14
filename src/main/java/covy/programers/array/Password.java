74dff3a268bc3bfa4e9021b57745b5b1c8bdb126
covy <junni802@naver.com>
2025-07-14 21:34:07 +0900

    Feat: add Password Test

--- src/main/java/covy/programers/array/Password.java
+++ src/main/java/covy/programers/array/Password.java
@@ -1,5 +1,23 @@
 package covy.programers.array;
 
 public class Password {
+  public String solution(String s, String skip, int index) {
+    String answer = "";
 
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      for (int j = 0; j < index; j++) {
+        c += 1;
+        if (c > 'z') {
+          c -= 26;
+        }
+        if (skip.contains(String.valueOf(c))) {
+          j--;
+        }
+      }
+      answer += c;
+    }
+
+    return answer;
+  }
 }
