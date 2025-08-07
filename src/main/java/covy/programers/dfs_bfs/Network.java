db40054dd81aa6de20422f09eb57dcf2f411384f
covy <junni802@naver.com>
2025-08-07 17:38:06 +0900

    Feat: add

--- src/main/java/covy/programers/dfs_bfs/Network.java
+++ src/main/java/covy/programers/dfs_bfs/Network.java
@@ -3,26 +3,28 @@ package covy.programers.dfs_bfs;
 public class Network {
 
     public int solution(int n, int[][] computers) {
-      int answer = 0;
-      boolean[] booleans = new boolean[n];
-      for (int i = 0; i < n; i++) {
-        if (!booleans[i]) {
-          dfs(i, computers, booleans);
-          answer++;
-        }
-      }
+     int answer = 0;
+     boolean[] booleans = new boolean[n];
 
-      return answer;
+     for (int i = 0; i < n; i++) {
+       if (!booleans[i]) {
+         dfs(i, computers, booleans);
+         answer++;
+       }
+     }
+
+     return answer;
     }
 
     public void dfs(int i, int[][] computers, boolean[] booleans) {
-      booleans[i] = true;
-      for (int j = 0; j < computers[i].length; j++) {
-        if(i != j && computers[i][j] == 1 && !booleans[j]) {
+      for (int j = 0; j < computers.length; j++) {
+        if (i != j && computers[i][j] == 1 && !booleans[j]) {
+          booleans[j] = true;
           dfs(j, computers, booleans);
         }
       }
     }
+
 
 
 }
