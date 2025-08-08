7882dea869e9ea0327bebef69324215195f01ef1
covy <junni802@naver.com>
2025-08-08 17:57:34 +0900

    feat: add Test Case

---
+++ src/test/java/covy/programers/dfs_bfs/TossTest.java
@@ -0,0 +1,25 @@
+package covy.programers.dfs_bfs;
+
+import org.junit.jupiter.api.Test;
+
+public class TossTest {
+
+  @Test
+  void test() {
+    int soulution = solution("123");
+    System.out.println(soulution);
+  }
+
+  public int solution(String number) {
+    int answer = -1;
+    char[] chars = number.toCharArray();
+
+    for(int i=0; i<chars.length - 2; i++) {
+      if (chars[i] == chars[i+1] && chars[i+1] == chars[i+2]) {
+        answer = Math.max(answer, Integer.parseInt((number).substring(i, i+3)));
+      }
+    }
+    return answer;
+  }
+
+}
