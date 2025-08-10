e92c717c53669ff5d3f895a176e94f597881546d
covy <junni802@naver.com>
2025-08-10 18:35:42 +0900

    feat: add [FuctionDevelop] Success Test

---
+++ src/test/java/covy/programers/stack/FunctionDevelopTest.java
@@ -0,0 +1,42 @@
+package covy.programers.stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class FunctionDevelopTest {
+
+  @Test
+  void test() {
+    int[] progress = {93, 30, 55};
+    int[] speeds = {1, 30, 5};
+
+    solution(progress, speeds);
+  }
+
+  public int[] solution(int[] progresses, int[] speeds) {
+    List<Integer> result = new ArrayList<>();
+    int day = 0; // 경과 일수
+    int count = 0; // 같은 날 배포할 기능 개수
+
+    for (int i = 0; i < progresses.length; i++) {
+      int workDays = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+
+      if (day < workDays) {
+        // 이전까지 배포할 기능이 있으면 기록
+        if (count > 0) result.add(count);
+
+        day = workDays; // 새로운 배포 시작일
+        count = 1;
+      } else {
+        count++;
+      }
+    }
+    result.add(count);
+
+    return result.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+}
