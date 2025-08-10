83494763d7cc3298f3ffbb7bb0a75d8835e954e2
covy <junni802@naver.com>
2025-08-10 19:01:43 +0900

    feat: add Success ValidParentheses

--- src/main/java/covy/programers/stack/ValidParentheses.java
+++ src/main/java/covy/programers/stack/ValidParentheses.java
@@ -2,4 +2,24 @@ package covy.programers.stack;
 
 public class ValidParentheses {
 
+  boolean solution(String s) {
+    int answer = 0;
+
+    char[] ch = s.toCharArray();
+
+    for(int i=0; i<ch.length; i++) {
+      if(ch[i] == '(') {
+        answer++;
+      } else if(ch[i] == ')') {
+        answer--;
+      }
+
+      if (answer < 0) {
+        return false;
+      }
+    }
+
+    return answer == 0;
+  }
+
 }
