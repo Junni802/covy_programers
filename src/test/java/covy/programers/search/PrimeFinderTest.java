b64c79521be7cd90d54f6c1d438fd8c5551007b0
covy <junni802@naver.com>
2025-08-22 07:03:40 +0900

    feat: add PrimeFinderTest Class

---
+++ src/test/java/covy/programers/search/PrimeFinderTest.java
@@ -0,0 +1,29 @@
+package covy.programers.search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PrimeFinderTest {
+
+  public int solution(String numbers) {
+    List<Integer> list = new ArrayList<>();
+
+    for (int i=0; i<numbers.length(); i++) {
+
+    }
+  }
+
+  /**
+   * 재귀호출
+   *
+   * @param str: 문제 안에서의 숫자 문자열
+   * @param tmp: 재귀호출로 인하여 만듫어지는 숫자 문자열
+   * @param m: 현재 몇번쨰 숫자를 구해야 하는지 알 수 있는 수
+   */
+  public void dfs(String str, String tmp, int m) {
+    if ()
+  }
+
+}
