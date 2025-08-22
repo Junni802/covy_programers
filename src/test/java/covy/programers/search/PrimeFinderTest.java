46525502e5c3a97c359c02ff9d82d33c5c02da24
covy <junni802@naver.com>
2025-08-22 09:30:54 +0900

    feat: add

--- src/test/java/covy/programers/search/PrimeFinderTest.java
+++ src/test/java/covy/programers/search/PrimeFinderTest.java
@@ -6,13 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 class PrimeFinderTest {
+  List<Integer> list = new ArrayList<>();
+  boolean[] resultYn = new boolean[7];
 
   public int solution(String numbers) {
-    List<Integer> list = new ArrayList<>();
+    int answer = 0;
 
     for (int i=0; i<numbers.length(); i++) {
-
+      dfs(numbers, "", i + 1);
     }
+
+    for(int i=0; i<list.size(); i++){
+      if(prime(list.get(i))) answer++;
+    }
+
+    return answer;
   }
 
   /**
@@ -23,7 +31,35 @@ class PrimeFinderTest {
    * @param m: 현재 몇번쨰 숫자를 구해야 하는지 알 수 있는 수
    */
   public void dfs(String str, String tmp, int m) {
-    if ()
+    // tmp의 사이즈가 원하는 자리수와 같을 경우
+    if (tmp.length() == m) {
+      int num = Integer.parseInt(tmp);
+      // list안에 해당 값이 없을 경우
+      if (!list.contains(num)) {
+        list.add(num);
+      }
+    }
+
+    for (int i=0; i<str.length(); i++) {
+      // 등록이 안된 번호인 경우
+      if (!resultYn[i]) {
+        resultYn[i] = true;
+        tmp += str.charAt(i);
+        dfs(str, tmp, m);
+        resultYn[i] = false;
+        tmp = tmp.substring(0, tmp.length()-1);
+      }
+    }
+  }
+
+  public boolean prime(int n) {
+    if(n<2) return false;
+
+    for(int i=2; i*i<=n; i++) {
+      if(n % i == 0) return false;
+    }
+
+    return true;
   }
 
 }
